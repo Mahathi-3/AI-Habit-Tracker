1139caf456c06447f54032e53e87f84c2dc3905b
mahathi052 <mattalakshmimahathi@gmail.com>
2026-05-25 13:46:31 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
+    const sonn = await mongoose.connect(uri);
+
+    console.log(`MongoDB connected: ${sonn.connection.host}`);
+
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
